74e36ddf16c9fd0c2ba23bfbf1596ec52306acbe
Krushil <krushilnaik96@gmail.com>
2023-06-15 14:45:57 -0400

    Plan for page indicators

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -18,52 +18,55 @@ function ProjectPage() {
   }
 
   return (
-    <div className="flex flex-wrap max-w-7xl mx-auto items-center justify-center gap-4 p-4">
-      {data?.projects.map((_p, i) => (
-        <button
-          key={i}
-          className="p-2 bg-rose-800 transition-all duration-300 hover:scale-110 rounded-lg w-80 md:w-96 flex flex-col gap-3"
-          onClick={() => {
-            setProjectImage(_p.desktopScreenshot?.url || "");
-            router.push(`/projects/${_p.slug}`);
-          }}
-        >
-          <motion.div
-            layoutId={_p.slug}
-            className="w-full aspect-video grid place-content-center bg-rose-600 rounded-lg text-xl"
+    <div className="flex flex-col items-center justify-evenly">
+      <div className="flex flex-wrap max-w-7xl mx-auto items-center justify-center gap-4 p-4">
+        {data?.projects.slice(0, 6).map((_p, i) => (
+          <button
+            key={i}
+            className="p-2 bg-rose-900 transition-all duration-300 hover:scale-110 rounded-lg w-80 md:w-96 flex flex-col gap-3"
+            onClick={() => {
+              setProjectImage(_p.desktopScreenshot?.url || "");
+              router.push(`/projects/${_p.slug}`);
+            }}
           >
-            {_p.desktopScreenshot?.url ? (
-              <Image
-                className="w-full aspect-video rounded-lg"
-                src={_p.desktopScreenshot?.url || ""}
-                width={_p.desktopScreenshot?.width || 1920}
-                height={_p.desktopScreenshot?.height || 1080}
-                alt={_p.title}
-              />
-            ) : (
-              _p.title
-            )}
-          </motion.div>
-          <div className="flex justify-between w-full">
-            <ul className="flex gap-1">
-              {_p.techStack.map(({ icon, ..._t }) => (
-                <li key={_p.title + _t.title}>
-                  {
-                    <Image
-                      src={icon?.url || ""}
-                      height={icon?.height || 400}
-                      width={icon?.width || 400}
-                      className="h-5 w-5"
-                      alt={_t.title}
-                    />
-                  }
-                </li>
-              ))}
-            </ul>
-            {_p.isWIP && <span>⚒️</span>}
-          </div>
-        </button>
-      ))}
+            <motion.div
+              layoutId={_p.slug}
+              className="w-full aspect-video grid place-content-center bg-rose-600 rounded-lg text-xl"
+            >
+              {_p.desktopScreenshot?.url ? (
+                <Image
+                  className="w-full aspect-video rounded-lg"
+                  src={_p.desktopScreenshot?.url || ""}
+                  width={_p.desktopScreenshot?.width || 1920}
+                  height={_p.desktopScreenshot?.height || 1080}
+                  alt={_p.title}
+                />
+              ) : (
+                _p.title
+              )}
+            </motion.div>
+            <div className="flex justify-between w-full">
+              <ul className="flex gap-1">
+                {_p.techStack.map(({ icon, ..._t }) => (
+                  <li key={_p.title + _t.title}>
+                    {
+                      <Image
+                        src={icon?.url || ""}
+                        height={icon?.height || 400}
+                        width={icon?.width || 400}
+                        className="h-5 w-5"
+                        alt={_t.title}
+                      />
+                    }
+                  </li>
+                ))}
+              </ul>
+              {_p.isWIP && <span>⚒️</span>}
+            </div>
+          </button>
+        ))}
+      </div>
+      <span>Page indicators coming soon</span>
     </div>
   );
 }
